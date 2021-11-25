860b0f16424a4f5bfe99d3e89ff5e0baa009d372
george-usynin-deriv <heorhi@deriv.com>
2021-11-25 09:53:34 +0300

    george/fixed sonarcloud, fixed types

--- src/components/common/CanvasMenu/CanvasMenu.tsx
+++ src/components/common/CanvasMenu/CanvasMenu.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React, { MouseEventHandler } from "react";
 import { PATHS } from "utils";
 import styles from "./CanvasMenu.module.scss";
